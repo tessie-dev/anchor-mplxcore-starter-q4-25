ba85df703caac9aaf8760031f478443f4518c37d
tcypig <tianyuan1007@gamil.com>
2026-02-05 21:23:28 -0800

    Implement Freeze and Thaw NFT instructions with account constraints and CPI integration

--- programs/anchor-mplxcore-q4-25/src/instructions/thaw_nft.rs
+++ programs/anchor-mplxcore-q4-25/src/instructions/thaw_nft.rs
@@ -7,15 +7,57 @@ use mpl_core::{
 
 use crate::{error::MPLXCoreError, state::CollectionAuthority};
 
-// #[derive(Accounts)]
-// pub struct ThawNft<'info> {
-//    // TODO
-// }
+#[derive(Accounts)]
+pub struct ThawNft<'info> {
+   // TODO
+   #[account(mut)]
+    pub authority: Signer<'info>,
 
-// impl<'info> ThawNft<'info> {
-//     pub fn thaw_nft(&mut self) -> Result<()> {
-//         // TODO
+    #[account(mut)]
+    /// CHECK: checked by mpl_core
+    pub asset: UncheckedAccount<'info>,
 
-//         Ok(())
-//     }
-// }
+    #[account(
+        mut,
+        constraint = collection.owner == &CORE_PROGRAM_ID @ MPLXCoreError::InvalidCollection,
+        constraint = !collection.data_is_empty() @ MPLXCoreError::CollectionNotInitialized
+    )]
+    /// CHECK: checked by mpl_core
+    pub collection: UncheckedAccount<'info>,
+
+    #[account(
+        seeds = [b"collection_authority", collection.key().as_ref()],
+        bump = collection_authority.bump,
+        constraint = collection_authority.creator == authority.key()
+            @ MPLXCoreError::NotAuthorized
+    )]
+    pub collection_authority: Account<'info, CollectionAuthority>,
+
+    #[account(address = CORE_PROGRAM_ID)]
+    /// CHECK: checked by mpl_core
+    pub core_program: UncheckedAccount<'info>,
+
+    pub system_program: Program<'info, System>,
+}
+
+impl<'info> ThawNft<'info> {
+    pub fn thaw_nft(&mut self) -> Result<()> {
+        // TODO
+        let signer_seeds: &[&[&[u8]]] = &[&[
+            b"collection_authority",
+            &self.collection.key().to_bytes(),
+            &[self.collection_authority.bump],
+        ]];
+
+        UpdatePluginV1CpiBuilder::new(&self.core_program.to_account_info())
+            .asset(&self.asset.to_account_info())
+            .collection(Some(&self.collection.to_account_info()))
+            .payer(&self.authority.to_account_info())
+            .authority(Some(&self.collection_authority.to_account_info()))
+            .system_program(&self.system_program.to_account_info())
+            .plugin(Plugin::FreezeDelegate(FreezeDelegate { frozen: false }))
+            .invoke_signed(signer_seeds)?;
+
+        Ok(())
+    }
+}
